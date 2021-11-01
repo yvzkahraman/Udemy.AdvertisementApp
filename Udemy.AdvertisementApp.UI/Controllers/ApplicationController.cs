45e4f7daa07955e7fbd94a8b41d95d471d2efa90
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2021-11-01 03:17:11 +0300

    completed

--- Udemy.AdvertisementApp.UI/Controllers/ApplicationController.cs
+++ Udemy.AdvertisementApp.UI/Controllers/ApplicationController.cs
@@ -30,5 +30,32 @@ namespace Udemy.AdvertisementApp.UI.Controllers
         {
             return View(new AdvertisementCreateDto());
         }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(AdvertisementCreateDto dto)
+        {
+            var response = await _advertisementService.CreateAsync(dto);
+            return this.ResponseRedirectAction(response, "List");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _advertisementService.GetByIdAsync<AdvertisementUpdateDto>(id);
+            return this.ResponseView(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(AdvertisementUpdateDto dto)
+        {
+            var response = await _advertisementService.UpdateAsync(dto);
+            return this.ResponseRedirectAction(response, "List");
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var response = await _advertisementService.RemoveAsync(id);
+            return this.ResponseRedirectAction(response, "List");
+        }
     }
 }
